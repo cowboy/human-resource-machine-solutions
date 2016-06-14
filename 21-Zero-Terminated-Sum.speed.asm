bb7fb6f754fec80f0c5b884c3cd644dc4d1f4d4c
Florian Bastien <fbastien@gmail.com>
2016-06-15 01:12:19 +0200

    Faster (63) floor 21

--- 21-Zero-Terminated-Sum.speed.asm
+++ 21-Zero-Terminated-Sum.speed.asm
@@ -1,26 +1,38 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
 
-    JUMP     c
 a:
-    COPYFROM 2
-b:
-    OUTBOX  
-c:
-    INBOX   
+    INBOX
+    JUMPZ    g
+    COPYTO   0
+    INBOX
+    JUMPZ    f
+    COPYTO   1
+    INBOX
+    JUMPZ    e
+    COPYTO   2
+    INBOX
+    JUMPZ    d
+    COPYTO   3
+    INBOX
+    JUMPZ    c
+    COPYTO   4
+    INBOX
     JUMPZ    b
-    COPYTO   2
+    COPYTO   5
+    INBOX
+    ADD      5
+b:
+    ADD      4
+c:
+    ADD      3
 d:
-    INBOX   
-    JUMPZ    a
     ADD      2
-    COPYTO   2
-    JUMP     d
+e:
+    ADD      1
+f:
+    ADD      0
+g:
+    OUTBOX
+    JUMP     a
 
-
-DEFINE LABEL 2
-eJyzZGBgUIp0DXePuOAXE7bVdWrwLfu1fsuswj0ZrG66z7Lxcs0KbHB9mBbttifnlYdtMVvg0toHgfYN
-8cFRnZYhF+fZBh5f2+Efuumgz9Stu5webdN24NiY6HB8LdBYBomkNPPWpM/W/kn52ctTXs1qSg1aypB+
-fO2bjN+b/mYu2vwrJ3TTo8LJK0RLSyd9KDVu+lD6OryohMcCpHdv0+QVjxs70qWaTOxnNNsZtLTu1WRq
-L9AQ7uDSFuiOdsvsnZIq158V+HXSOcdVU3rtYmb6WkfMWWaVOdfB9sU8Zy+uhf9ruBa2zE1bMHkFwygY
-BaMAJwAApMNkKA;
 
